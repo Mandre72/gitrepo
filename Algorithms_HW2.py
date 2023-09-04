033543a5f708c12dbdb29ef1f06b63964377612e
Mandre72 <Matthew.andre@yahoo.com>
2023-09-04 11:46:15 -0500

    Algorithms HW2

--- Algorithms_HW2.py
+++ Algorithms_HW2.py
@@ -1,36 +1,114 @@
-#def split_in_half(s):
-#    length = len(s)
-#    half = length // 2
-#    add = 0
-#    if length % 2:
-#        add = 1
-#    left = s[:half + add]
-#    right = s[half + add:]
-#    return right + left
+#Level 1
+#Task1
+def reverse_integer(n: int) -> int:
 
-# test_even = "aaabbb" # bbbaaa
-# test_odd = "aaabbbb" #bbbaaab
-# result_even = split_in_half(test_even)
-# print(result_even)
-# result_odd = split_in_half(test_odd)
-# print(result_odd)
+    num_str = str(n)
+
+    if num_str[0] == '-':
+
+        reversed_str = '-' + num_str[1:][::-1]
+    else:
+
+        reversed_str = num_str[::-1]
+
+    reversed_int = int(reversed_str)
+
+    return reversed_int
 
 
-def uni_char(s):
-    chars = set()
+original_num = -189
+reversed_num = reverse_integer(original_num)
+print(f"Original: {original_num}, Reversed: {reversed_num}")
+
+
+#Task2
+def are_anagrams(s1: str, s2: str) -> bool:
+
+    s1 = s1.lower()
+    s2 = s2.lower()
+
+    sorted_s1 = sorted(s1)
+    sorted_s2 = sorted(s2)
+
+    return sorted_s1 == sorted_s2
+
+
+word1 = "race"
+word2 = "care"
+print(are_anagrams(word1, word2))  # Should return True
+
+word1 = "hEArt"
+word2 = "earth"
+print(are_anagrams(word1, word2))  # Should return True
+
+word1 = "rattle"
+word2 = "battle"
+print(are_anagrams(word1, word2))  # Should return False
+
+
+#Level2
+#Task3
+def reverse_words(sentence: str) -> str:
+
+    words = sentence.split()
+
+    reversed_words = []
+
+    for word in words:
+
+        reversed_word = word[::-1]
+
+        reversed_words.append(reversed_word)
+
+    reversed_sentence = " ".join(reversed_words)
+
+    return reversed_sentence
+
+
+sentence1 = "Hello World"
+print(reverse_words(sentence1))  # Should return "olleH dlroW"
+
+sentence2 = "mistertwister"
+print(reverse_words(sentence2))  # Should return "retsiwtretsim"
+
+
+#Task4
+def repeat_digits(s: str) -> str:
+    result = ""
 
     for char in s:
-        if char in chars:
-            return False
-        else:
-            chars.add(char)
+        current_num = int(char)
+        repeated_char = char * current_num
+        result += repeated_char
 
-    return True
+    return result  #
 
 
-test_pos = "abcde"
-test_neg = "abcdb"
-result_pos = uni_char(test_pos)
-print(result_pos)  # True
-result_neg = uni_char(test_neg)
-print(result_neg)  # False
+string1 = "312"
+print(repeat_digits(string1))  # Should return "333122"
+
+string2 = "102"
+print(repeat_digits(string2))  # Should return "122"
+
+
+#Task5
+def shortcut(s: str) -> str:
+
+    result = ""
+
+    vowels = set("aeiou")
+
+    for char in s:
+
+        if char not in vowels:
+
+            result += char
+
+    return result
+
+
+string1 = "hello"
+print(shortcut(string1))  # Should return "hll"
+
+string2 = "goodbye"
+print(shortcut(string2))  # Should return "gdby"
